0f7044ac8ec9fe0454f8d7a7beb8da20575e26d2
Tone@Silvren <kinostl@gmail.com>
2020-01-23 23:52:28 -0500

    Converted to node

---
+++ discord_receive.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const emoji = require('node-emoji')
+const axios = require('axios')
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('message', message => {
+        // don't respond to ourselves
+        if(message.author == client.user)return
+        // don't respond to webhooks
+        if(message.author.discriminator == '0000')return
+        const execstring=`cemit(${message.channel.name},escape(${message.member.displayName}#${message.author.discriminator}->${emoji.unemojify(message.cleanContent)}))`
+        const headers={"Exec":execstring, "Encode":"Yes"}
+        console.log(execstring)
+        //requests.post(,headers=headers,auth=("#50","SomethingSecure"))
+	axios({
+		method: 'post',
+		url: 'http://localhost:60601',
+		headers: headers,
+		auth:{
+			username: '#50',
+			password: 'SomethingSecure'
+		}
+	})
+})
+
+client.login('NjU3NzUxMDM2OTc0NDY1MDcy.Xf1weA.9MJzhdsLT_cM220pH40gaTJoABQ')
---
+++ discord_send.js
@@ -0,0 +1,32 @@
+const hooks={
+	'newbie':'https://discordapp.com/api/webhooks/665537445445763083/5Dkklgi2U8TIuc3TEUK4_53sF9f59hamJojpzUIrHJmUtZ0sBLgKRe4Ki4Naio6SdJfQ',
+	'public':'https://discordapp.com/api/webhooks/657282464682606595/iljMIKzVlGOPFLB3RmQwgOQAYQc2nucLfXR7fn0EkijL83SmV0ywvmjm99GGigQdESz3',
+	'ad-workshop':'https://discordapp.com/api/webhooks/665537954256650250/9cDHyyvzceCYS4LgtIp23JCwYVH5OtQIAYwaE-G65VigA25zSRyrEWzrpx0YYnQIAaFK',
+	'workshop':'https://discordapp.com/api/webhooks/665538090948886543/o7nNdtte-x3O3YNzzCNp7n4UafMBG_z364-y_A4CsjlEAwHzZCoF245Fymj0YWWyN6cY',
+	'test':'https://discordapp.com/api/webhooks/670032397567524864/y25MIeeJLJ3fQjGtGqoP341kmKXWm7mnnN5QVWFXXxwik0h7nMq7_YuYXBwYrhAXZqIq',
+}
+
+const axios = require('axios')
+const debug = require('debug')('relay')
+const timer = require('debug')('timer')
+timer('start')
+debug('relay start')
+
+const username = process.env['MUSHQ_A']
+const message = process.env['MUSHQ_B']
+const channel = process.env['MUSHQ_C']
+const shouldFire = (username != "ApiTool" && !message.endsWith('connected.'))
+
+
+if(shouldFire){
+	const hook = hooks[channel]
+	debug('variables set')
+	debug('firing')
+	axios.post(hook,{
+		'username':username,
+		'content':message
+	})
+	debug('fired')
+}
+debug('closing script')
+timer('end')
