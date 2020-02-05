96ffc3992e9b17c9425cbbeb2abb914964442724
Minty Cream <kinostl@gmail.com>
2020-02-05 14:25:41 -0500

    Update discord_send.js

--- discord_send.js
+++ discord_send.js
@@ -1,9 +1,5 @@
 const hooks={
-	'newbie':'https://discordapp.com/api/webhooks/665537445445763083/5Dkklgi2U8TIuc3TEUK4_53sF9f59hamJojpzUIrHJmUtZ0sBLgKRe4Ki4Naio6SdJfQ',
-	'public':'https://discordapp.com/api/webhooks/657282464682606595/iljMIKzVlGOPFLB3RmQwgOQAYQc2nucLfXR7fn0EkijL83SmV0ywvmjm99GGigQdESz3',
-	'ad-workshop':'https://discordapp.com/api/webhooks/665537954256650250/9cDHyyvzceCYS4LgtIp23JCwYVH5OtQIAYwaE-G65VigA25zSRyrEWzrpx0YYnQIAaFK',
-	'workshop':'https://discordapp.com/api/webhooks/665538090948886543/o7nNdtte-x3O3YNzzCNp7n4UafMBG_z364-y_A4CsjlEAwHzZCoF245Fymj0YWWyN6cY',
-	'test':'https://discordapp.com/api/webhooks/670032397567524864/y25MIeeJLJ3fQjGtGqoP341kmKXWm7mnnN5QVWFXXxwik0h7nMq7_YuYXBwYrhAXZqIq',
+	'ashcom-channel-name':'https://discord.channel.webhook',
 }
 
 const md5 = require('md5')
