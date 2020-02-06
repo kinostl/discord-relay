6298d2d544a91e639c39e96f26b377f6d4cf9a79
Minty Cream <kinostl@gmail.com>
2020-02-05 19:08:04 -0500

    Update discord_receive.js

--- discord_receive.js
+++ discord_receive.js
@@ -4,7 +4,7 @@ const emoji = require('node-emoji')
 const axios = require('axios')
 
 client.on('ready', () => {
-	console.log(`Logged in as ${client.user.tag}!`)
+        console.log(`Logged in as ${client.user.tag}!`)
 })
 
 client.on('message', message => {
@@ -12,16 +12,15 @@ client.on('message', message => {
         if(message.author == client.user)return
         // don't respond to webhooks
         if(message.author.discriminator == '0000')return
-	const name = `<${message.member.displayName}#${message.author.discriminator}>`
-	let post = emoji.unemojify(message.cleanContent)
-	post = post.replace(/\r|\n|\r\n|\n\r/g,'%r')
-	if(message.attachments.array().length > 0){
-		post = message.attachments.reduce((accumulate, attachment)=>`${accumulate}%r${attachment.url}`,`${post}%r- Attachments -`)
-	}
-        const execstring=`cemit(${message.channel.name},escape(ansi(hm,%[D%])%b${name}%b${post}))`
+        const name = `<${message.author.username}#${message.author.discriminator}>`
+        let post = emoji.unemojify(message.cleanContent)
+        post = post.replace(/\r|\n|\r\n|\n\r/g,'%r')
+        if(message.attachments.array().length > 0){
+                post = message.attachments.reduce((accumulate, attachment)=>`${accumulate}%r${attachment.url}`,`${post}%r- Attachments -`)
+        }
+        const execstring=`@cemit discord=%ch%cm[D]%cn ${name} ${post}`
         const headers={"Exec":execstring, "Encode":"Yes"}
-        console.log(execstring)
-        //requests.post(,headers=headers,auth=("#50","SomethingSecure"))
+
 	axios({
 		method: 'post',
 		url: 'http://0.0.0.0:60601',
